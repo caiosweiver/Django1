819a0e2b155b1678d295508e637572ab77e9b368
Rulinhu <caioscarvalho0055@gmail.com>
2023-08-11 16:51:18 -0300

    ab

--- NAMEOFPROEJECT/myapp/models.py
+++ NAMEOFPROEJECT/myapp/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from ckeditor.fields import RichTextField
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    content = models.TextField()
+    content = RichTextField(blank=True, null=True)
+    #content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     created_at = models.DateTimeField(auto_now_add=True)
 
